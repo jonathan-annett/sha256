03a6562b2bf4f34f46279d92f2d79a98d998aef3
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-15 11:12:44 +1100

    changed it to return object

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ fa_path = path.join(__dirname,"node_modules","font-awesome");
 function font_awesome_express(app,url){
     url = url||"/font-awesome";
     app.use(url,express.static(fa_path));
-    font_awesome_express.url_base = url;
-    font_awesome_express.url = path.join(url,"css","font-awesome.css");
-    font_awesome_express.url_min = path.join(url,"css","font-awesome.min.css");
+    return {
+        url_base : urlm
+        url : path.join(url,"css","font-awesome.css");
+        url_min : path.join(url,"css","font-awesome.min.css");
 }
 module.exports = font_awesome_express;
