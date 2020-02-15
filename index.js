e7b7dc8ee7a824fe17d0dbfc47b142c8f10d90b0
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-15 11:42:23 +1100

    added relative path fixup

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ fs      = require("fs"),
 express = require("express"),
 fa_path = path.join(__dirname,"node_modules","font-awesome");
 
+if (!fs.existsSync(fa_path)) {
+    fa_path = path.join(__dirname,"..","font-awesome");
+}
+
 
 function font_awesome_express(app,url){
     url = url||"/font-awesome";
+    console.log(fa_path);
     app.use(url,express.static(fa_path));
     return {
         url_base : url,
