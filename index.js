77b58450f5a7400ac875dfc12ed451fe0b775fbc
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-15 11:06:37 +1100

    added url and min_url

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-
 var 
 
-path = require("path"),
-fs=require("fs"),
-express=require("express"),
-fa_path=path.join(__dirname,"node_modules","font-awesome");
+path    = require("path"),
+fs      = require("fs"),
+express = require("express"),
+fa_path = path.join(__dirname,"node_modules","font-awesome");
 
 
 function font_awesome_express(app,url){
-    app.use(url||"/font-awesome",express.static(fa_path));
+    url = url||"/font-awesome";
+    app.use(url,express.static(fa_path));
+    font_awesome_express.url_base = url;
+    font_awesome_express.url = path.join(url,"css","font-awesome.css");
+    font_awesome_express.url_min = path.join(url,"css","font-awesome.min.css");
 }
-
 module.exports = font_awesome_express;
